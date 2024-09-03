e6bbcc59d3ae81c451d7eac6857836b7c1c88614
zyxwvutsujin <rhcwnl1904@naver.com>
2024-09-03 17:23:02 +0900

    Delete androidPhone directory

--- androidPhone/src/main/java/bitcfull/androidphone/repository/PhoneRepository.java
+++
@@ -1,7 +0,0 @@
-package bitcfull.androidphone.repository;
-
-import bitcfull.androidphone.model.Phone;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface PhoneRepository extends JpaRepository<Phone, Long> {
-}
