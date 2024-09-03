e348bff392d28bac8fe7a3b52cdd47c77a9b43dd
moranengyo <moranengyo@gmail.com>
2024-09-03 19:03:49 +0900

    <UserRepository>에 아이디 중복 확인 추가

--- Moodo_Spring/src/main/java/bitcfull/moodo_spring/repository/UserRepository.java
+++ Moodo_Spring/src/main/java/bitcfull/moodo_spring/repository/UserRepository.java
@@ -5,4 +5,5 @@ import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface UserRepository extends JpaRepository<MooDoUser, String> {
     // 나중에 커스텀 쿼리 추가 할거잇음하기
+  int countById(String id);
 }
