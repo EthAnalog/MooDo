3afb2ea15630ca4c7cdb4b9cf08edc461c79ff62
zyxwvutsujin <rhcwnl1904@naver.com>
2024-09-03 17:23:09 +0900

    Delete android_member directory

--- android_member/src/main/java/bitcfull/android_member/repository/MemberRepository.java
+++
@@ -1,7 +0,0 @@
-package bitcfull.android_member.repository;
-
-import bitcfull.android_member.model.Member;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface MemberRepository extends JpaRepository<Member, Long> {
-}
