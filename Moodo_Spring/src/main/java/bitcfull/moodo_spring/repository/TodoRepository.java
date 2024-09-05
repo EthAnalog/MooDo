ab140a8e01d1c06f0c1e39189e87c146ca35f0b2
zyxwvutsujin <rhcwnl1904@naver.com>
2024-09-05 14:44:03 +0900

    일정 날짜 검색 시 중간 날짜도 나오도록 업데이트

--- Moodo_Spring/src/main/java/bitcfull/moodo_spring/repository/TodoRepository.java
+++ Moodo_Spring/src/main/java/bitcfull/moodo_spring/repository/TodoRepository.java
@@ -3,15 +3,10 @@ package bitcfull.moodo_spring.repository;
 import bitcfull.moodo_spring.model.MooDoTodo;
 import org.springframework.data.jpa.repository.JpaRepository;
 
-import java.time.LocalDate;
 import java.time.LocalDateTime;
 import java.util.List;
-import java.util.Optional;
 
 public interface TodoRepository extends JpaRepository<MooDoTodo, Long> {
-
-    // 기존의 findByUserIdAndDate 으로 할 경우, Date를 Todo에서 찾을 수 없다는 에러 발생, startDate로 변경
-    List<MooDoTodo> findByUserIdAndStartDate(String userId, LocalDateTime startDate); // 사용자와 사용자의 날짜로 할 일 목록을 가져오는 메서드
-
-    Optional<MooDoTodo> findById(Long id); // 할 일 조회
+    // 특정 날짜가 startDate와 endDate 범위에 속하는 할 일 목록 조회
+    List<MooDoTodo> findByUserIdAndStartDateLessThanEqualAndEndDateGreaterThanEqual(String userId, LocalDateTime startDate, LocalDateTime endDate);
 }
