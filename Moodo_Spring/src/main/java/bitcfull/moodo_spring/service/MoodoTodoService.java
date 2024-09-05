0d967bf7609efedb107197a755d2cbe42eefd8de
zyxwvutsujin <rhcwnl1904@naver.com>
2024-09-05 14:44:33 +0900

    날짜 검색 시 중간 날짜도 검색 결과 나오게함

--- Moodo_Spring/src/main/java/bitcfull/moodo_spring/service/MoodoTodoService.java
+++ Moodo_Spring/src/main/java/bitcfull/moodo_spring/service/MoodoTodoService.java
@@ -3,13 +3,12 @@ package bitcfull.moodo_spring.service;
 import bitcfull.moodo_spring.model.MooDoTodo;
 import bitcfull.moodo_spring.model.MooDoUser;
 import bitcfull.moodo_spring.repository.TodoRepository;
-import org.hibernate.annotations.Array;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import java.time.LocalDate;
 import java.time.LocalDateTime;
-import java.time.format.DateTimeFormatter;
+import java.time.LocalTime;
 import java.util.List;
 import java.util.Optional;
 
@@ -45,7 +44,6 @@ public class MoodoTodoService {
 
     //    할일 완료 체크
     public MooDoTodo updateCheck(Long id, String tdCheck) {
-        System.out.println("Received tdCheck value in service: " + tdCheck); // 로그 추가
         MooDoTodo updateTodo = todoRepository.findById(id)
                 .orElseThrow(() -> new RuntimeException("에러 발생"));
         // 상태 체크
@@ -57,23 +55,24 @@ public class MoodoTodoService {
         }
     }
 
-    // 특정 할 일 조회 (검색 통해서 조회? 나중에 필요없으면 빼기)
+    // 특정 할 일 조회
     public Optional<MooDoTodo> findById(Long id) {
         return todoRepository.findById(id);
     }
 
-    // 특정 사용자가 지정한 날짜에 등록한 할 일 목록 조회 (달력에서 날짜 터치하고 해당 날짜 리스트 조회)
-    public List<MooDoTodo> findByUserIdAndStartDate(String userId, String startDate) {
-        DateTimeFormatter formatter = DateTimeFormatter.ofPattern("yyyy/MM/dd");
-        LocalDateTime localDateTime = LocalDateTime.parse(startDate, formatter);
-        return todoRepository.findByUserIdAndStartDate(userId, localDateTime);
-    }
+    // 특정 사용자가 지정한 날짜에 등록한 할 일 목록 조회
+    public List<MooDoTodo> findByUserIdAndDate(String userId, LocalDate date) {
+        // 하루의 시작 시간
+        LocalDateTime startOfDay = date.atStartOfDay();
+        // 하루의 끝 시간
+        LocalDateTime endOfDay = date.atTime(LocalTime.MAX);
 
+        // 특정 날짜가 일정의 시작일과 종료일 사이에 있는 할 일 목록 조회
+        return todoRepository.findByUserIdAndStartDateLessThanEqualAndEndDateGreaterThanEqual(userId, startOfDay, endOfDay);
+    }
 
     // 할 일 삭제
     public void delete(Long id) {
         todoRepository.deleteById(id);
     }
-
-
 }
