818d720210cc5390afab12108f2452a1896c6832
zyxwvutsujin <rhcwnl1904@naver.com>
2024-09-06 14:13:13 +0900

    date 타입으로 수정

--- Moodo_Spring/src/main/java/bitcfull/moodo_spring/repository/TodoRepository.java
+++ Moodo_Spring/src/main/java/bitcfull/moodo_spring/repository/TodoRepository.java
@@ -3,10 +3,15 @@ package bitcfull.moodo_spring.repository;
 import bitcfull.moodo_spring.model.MooDoTodo;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.time.LocalDate;
 import java.time.LocalDateTime;
+import java.util.Date;
 import java.util.List;
+import java.util.Optional;
 
 public interface TodoRepository extends JpaRepository<MooDoTodo, Long> {
-    // 특정 날짜가 startDate와 endDate 범위에 속하는 할 일 목록 조회
-    List<MooDoTodo> findByUserIdAndStartDateLessThanEqualAndEndDateGreaterThanEqual(String userId, LocalDateTime startDate, LocalDateTime endDate);
+
+    // 특정 날짜가 startDate 와 EndDate 사이에 있는 일정 모두 조회
+    List<MooDoTodo> findByUserIdAndStartDateLessThanEqualAndEndDateGreaterThanEqual(String userId, Date startDate, Date endDate) throws Exception;
+    Optional<MooDoTodo> findById(Long id); // 할 일 조회
 }
