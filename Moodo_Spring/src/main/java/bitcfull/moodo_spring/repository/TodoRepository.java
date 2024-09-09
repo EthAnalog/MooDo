d4a95839203c1dae169c68f22bcc5d6b1b6b4209
zyxwvutsujin <rhcwnl1904@naver.com>
2024-09-09 12:14:36 +0900

    한달동안 td Y / N 조회 추가

--- Moodo_Spring/src/main/java/bitcfull/moodo_spring/repository/TodoRepository.java
+++ Moodo_Spring/src/main/java/bitcfull/moodo_spring/repository/TodoRepository.java
@@ -16,6 +16,14 @@ public interface TodoRepository extends JpaRepository<MooDoTodo, Long> {
             @Param("startDate") String startDate,
             @Param("endDate") String endDate);
 
+    // 선택한 날짜 할일 조회 및 td_check 확인
+    @Query("SELECT t FROM MooDoTodo t WHERE t.user.id = :userId AND t.startDate <= :endDate AND t.endDate >= :startDate AND t.tdCheck = :check")
+    List<MooDoTodo> findByUserIdAndStartDateLessThanEqualAndEndDateGreaterThanEqualAndTdCheck(
+            @Param("userId") String userId,
+            @Param("startDate") String startOfDay,
+            @Param("endDate") String endOfDay,
+            @Param("check") String check);
+
     Optional<MooDoTodo> findById(Long id); // 할 일 조회
 
     @Query("SELECT COUNT(t) FROM MooDoTodo t WHERE t.user.id = :userId AND t.startDate BETWEEN :startDate AND :endDate")
