9fa4335140cf9aa43c3b8928b15537e29ad5f7c8
zyxwvutsujin <rhcwnl1904@naver.com>
2024-09-09 10:57:11 +0900

    date -> string 수정

--- Moodo_Spring/src/main/java/bitcfull/moodo_spring/model/MooDoTodo.java
+++ Moodo_Spring/src/main/java/bitcfull/moodo_spring/model/MooDoTodo.java
@@ -1,14 +1,11 @@
 package bitcfull.moodo_spring.model;
 
 
-import bitcfull.moodo_spring.MoodoSpringApplication;
-import com.fasterxml.jackson.annotation.JsonFormat;
+
 import jakarta.persistence.*;
 import lombok.Getter;
 import lombok.Setter;
-import org.hibernate.annotations.ColumnDefault;
 
-import java.time.LocalDateTime;
 import java.util.Date;
 
 @Getter @Setter
@@ -28,10 +25,10 @@ public class MooDoTodo {
     private String tdList; // 할일 목록
 
     @Column(nullable = false)
-    private Date startDate; // 일정 시작일
+    private String startDate; // 일정 시작일
 
     @Column(nullable = false)
-    private Date endDate; // 일정 마지막일
+    private String endDate; // 일정 마지막일
 
     // Boolean으로 할 경우, Service 내 if문에서 에러 발생
     // 길이 1의 String으로 형변환 및 기본값 N으로 설정
