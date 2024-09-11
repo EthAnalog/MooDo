38bd38c17aa19576e37dca23c8dec2e1aff96b02
moranengyo <moranengyo@gmail.com>
2024-09-11 17:22:36 +0900

    <Aejin> 생일 표시 구현 병합

--- MooDo/app/src/main/java/com/example/moodo/mode/MainActivity_MooDo.kt
+++ MooDo/app/src/main/java/com/example/moodo/mode/MainActivity_MooDo.kt
@@ -30,220 +30,225 @@ import java.util.Date
 import java.util.Locale
 
 class MainActivity_MooDo : AppCompatActivity() {
-    lateinit var binding:ActivityMainMooDoBinding
-    override fun onCreate(savedInstanceState: Bundle?) {
-        super.onCreate(savedInstanceState)
-        enableEdgeToEdge()
-        binding = ActivityMainMooDoBinding.inflate(layoutInflater)
-        setContentView(binding.root)
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
-            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
-            insets
-        }
+  lateinit var binding:ActivityMainMooDoBinding
+  var user:MooDoUser? = null
+  override fun onCreate(savedInstanceState: Bundle?) {
+    super.onCreate(savedInstanceState)
+    enableEdgeToEdge()
+    binding = ActivityMainMooDoBinding.inflate(layoutInflater)
+    setContentView(binding.root)
+    ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
+      val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
+      v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
+      insets
+    }
 
-        // 사용자 id
-        val userId = intent.getStringExtra("id").toString()
-        // 선택한 날짜 저장할 TextView 변수
-        val saveDate = binding.saveDate
+    // 사용자 id
+    val userId = intent.getStringExtra("id").toString()
+    val userAge = intent.getStringExtra("age").toString()
 
-        // tdAdapter
-        val todoAdapter = CalendarToDoAdapter()
-        binding.todoListLayout.adapter = todoAdapter
-        binding.todoListLayout.layoutManager = LinearLayoutManager(this)
+    Log.d("MooDoLog UserInfo", userAge)
 
-        // custom calendar 연결
-        val monthListManager = LinearLayoutManager(this, LinearLayoutManager.HORIZONTAL, false)
-        val monthAdapter = MonthAdapter(userId).apply {
-            // 날짜 선택
-            onDaySelectedListener = object :MonthAdapter.OnDaySelectedListener{
-                override fun onDaySelected(date: String) {
-                    Log.d("MooDoLog Id", userId)
-                    Log.d("MooDoLog day", date)
+    // 선택한 날짜 저장할 TextView 변수
+    val saveDate = binding.saveDate
 
-                    MooDoClient.retrofit.getTodoListN(userId, date).enqueue(object :retrofit2.Callback<List<MooDoToDo>>{
-                        override fun onResponse(
-                            call: Call<List<MooDoToDo>>,
-                            response: Response<List<MooDoToDo>>
-                        ) {
-                            if (response.isSuccessful) {
-                                val todoList = response.body() ?: mutableListOf()
+    // tdAdapter
+    val todoAdapter = CalendarToDoAdapter()
+    binding.todoListLayout.adapter = todoAdapter
+    binding.todoListLayout.layoutManager = LinearLayoutManager(this)
 
-                                todoAdapter.todoList.clear()
-                                todoAdapter.todoList.addAll(todoList)
-                                todoAdapter.notifyDataSetChanged()
-                            }else {
-                                Log.d("MooDoLog", "Response is not successful: ${response.code()}")
-                            }
-                        }
-                        override fun onFailure(call: Call<List<MooDoToDo>>, t: Throwable) {
-                            Log.d("MooDoLog getTodo Fail", t.toString())
-                        }
-                    })
-                    saveDate.text = date
-                    Log.d("MooDoLog saveDate", saveDate.text.toString())
-                }
+    // custom calendar 연결
+    val monthListManager = LinearLayoutManager(this, LinearLayoutManager.HORIZONTAL, false)
+    val monthAdapter = MonthAdapter(userId, userAge).apply {
+      // 날짜 선택
+      onDaySelectedListener = object :MonthAdapter.OnDaySelectedListener{
+        override fun onDaySelected(date: String) {
+          Log.d("MooDoLog Id", userId)
+          Log.d("MooDoLog day", date)
+
+          MooDoClient.retrofit.getTodoListN(userId, date).enqueue(object :retrofit2.Callback<List<MooDoToDo>>{
+            override fun onResponse(
+              call: Call<List<MooDoToDo>>,
+              response: Response<List<MooDoToDo>>
+            ) {
+              if (response.isSuccessful) {
+                val todoList = response.body() ?: mutableListOf()
+
+                todoAdapter.todoList.clear()
+                todoAdapter.todoList.addAll(todoList)
+                todoAdapter.notifyDataSetChanged()
+              }else {
+                Log.d("MooDoLog", "Response is not successful: ${response.code()}")
+              }
             }
-        }
-        // custom calendar 연결
-        binding.calendarCustom.apply {
-            layoutManager = monthListManager
-            adapter = monthAdapter
-            scrollToPosition(Int.MAX_VALUE / 2)
-        }
-
-        val snap = PagerSnapHelper()
-        snap.attachToRecyclerView(binding.calendarCustom)
-
-        // tdList 수정, 저장, 삭제, 완료 후 tdList update
-        val activityToDoListUpdate = registerForActivityResult(ActivityResultContracts.StartActivityForResult()) { result ->
-            if (result.resultCode == RESULT_OK) {
-                val update = result.data?.getBooleanExtra("update", false) ?: false
-                if (update) {
-                    val date = saveDate.text.toString()
-                    refreshTodoList(date)
-                    monthAdapter.notifyDataSetChanged()
-                }
+            override fun onFailure(call: Call<List<MooDoToDo>>, t: Throwable) {
+              Log.d("MooDoLog getTodo Fail", t.toString())
             }
+          })
+          saveDate.text = date
+          Log.d("MooDoLog saveDate", saveDate.text.toString())
         }
-        // to do list 작성 및 수정, 삭제
-        // recyclerView 클릭 시에도 페이지 이동
-        todoAdapter.onItemClickLister = object :CalendarToDoAdapter.OnItemClickLister {
-            override fun onItemClick(pos: Int) {
-                val intent = Intent(this@MainActivity_MooDo, MainActivity_ToDo::class.java)
-                val selectDate = saveDate.text.toString()
+      }
+    }
+    // custom calendar 연결
+    binding.calendarCustom.apply {
+      layoutManager = monthListManager
+      adapter = monthAdapter
+      scrollToPosition(Int.MAX_VALUE / 2)
+    }
 
-                intent.putExtra("userId", userId)
-                intent.putExtra("selectDate", selectDate)
+    val snap = PagerSnapHelper()
+    snap.attachToRecyclerView(binding.calendarCustom)
 
-                // startActivity(intent)
-                activityToDoListUpdate.launch(intent)
-            }
-
+    // tdList 수정, 저장, 삭제, 완료 후 tdList update
+    val activityToDoListUpdate = registerForActivityResult(ActivityResultContracts.StartActivityForResult()) { result ->
+      if (result.resultCode == RESULT_OK) {
+        val update = result.data?.getBooleanExtra("update", false) ?: false
+        if (update) {
+          val date = saveDate.text.toString()
+          refreshTodoList(date)
+          monthAdapter.notifyDataSetChanged()
         }
-        binding.btnWrite.setOnClickListener {
-            val intent = Intent(this, MainActivity_ToDo::class.java)
-            val selectDate = saveDate.text.toString()
+      }
+    }
+    // to do list 작성 및 수정, 삭제
+    // recyclerView 클릭 시에도 페이지 이동
+    todoAdapter.onItemClickLister = object :CalendarToDoAdapter.OnItemClickLister {
+      override fun onItemClick(pos: Int) {
+        val intent = Intent(this@MainActivity_MooDo, MainActivity_ToDo::class.java)
+        val selectDate = saveDate.text.toString()
 
-            intent.putExtra("userId", userId)
-            intent.putExtra("selectDate", selectDate)
+        intent.putExtra("userId", userId)
+        intent.putExtra("selectDate", selectDate)
 
-            // startActivity(intent)
-            activityToDoListUpdate.launch(intent)
+        // startActivity(intent)
+        activityToDoListUpdate.launch(intent)
+      }
+
+    }
+    binding.btnWrite.setOnClickListener {
+      val intent = Intent(this, MainActivity_ToDo::class.java)
+      val selectDate = saveDate.text.toString()
+
+      intent.putExtra("userId", userId)
+      intent.putExtra("selectDate", selectDate)
+
+      // startActivity(intent)
+      activityToDoListUpdate.launch(intent)
+    }
+
+    // 감정 작성 후 mode update
+    val activityMoodListUpdate = registerForActivityResult(ActivityResultContracts.StartActivityForResult()) {result ->
+      if (result.resultCode == RESULT_OK) {
+        val update = result.data?.getBooleanExtra("update", false) ?: false
+        if (update) {
+          monthAdapter.notifyDataSetChanged()
         }
+      }
+    }
+    // mode 작성
+    binding.moodWriteBtn.setOnClickListener {
+      val intent = Intent(this, MainActivity_ModeWrite::class.java)
+      val selectDate = saveDate.text.toString()
 
-        // 감정 작성 후 mode update
-        val activityMoodListUpdate = registerForActivityResult(ActivityResultContracts.StartActivityForResult()) {result ->
-            if (result.resultCode == RESULT_OK) {
-                val update = result.data?.getBooleanExtra("update", false) ?: false
-                if (update) {
-                    monthAdapter.notifyDataSetChanged()
-                }
-            }
+      val dateFormat = SimpleDateFormat("yyyy-MM-dd", Locale.getDefault())
+
+      try {
+        val userSelected = dateFormat.parse(selectDate)!!
+        val today = Date()
+
+        if (userSelected.after(today)) {
+          // 오늘보다 미래인 경우
+          AlertDialog.Builder(binding.root.context)
+            .setMessage("선택한 날짜가 오늘보다 이후입니다. 오늘까지의 일기만 작성할 수 있어요.")
+            .setPositiveButton("확인", null)
+            .show()
         }
-        // mode 작성
-        binding.moodWriteBtn.setOnClickListener {
-            val intent = Intent(this, MainActivity_ModeWrite::class.java)
-            val selectDate = saveDate.text.toString()
+        else {
+          MooDoClient.retrofit.userMoodListCheck(userId, selectDate).enqueue(object:retrofit2.Callback<Boolean> {
+            override fun onResponse(call: Call<Boolean>, response: Response<Boolean>) {
+              if (response.isSuccessful) {
+                if (response.body() == true) {
+                  intent.putExtra("userId", userId)
+                  intent.putExtra("selectDate", selectDate)
+                  val stats = "insert"
+                  intent.putExtra("stats", stats)
 
-            val dateFormat = SimpleDateFormat("yyyy-MM-dd", Locale.getDefault())
-
-            try {
-                val userSelected = dateFormat.parse(selectDate)!!
-                val today = Date()
-
-                if (userSelected.after(today)) {
-                    // 오늘보다 미래인 경우
-                    AlertDialog.Builder(binding.root.context)
-                        .setMessage("선택한 날짜가 오늘보다 이후입니다. 오늘까지의 일기만 작성할 수 있어요.")
-                        .setPositiveButton("확인", null)
-                        .show()
+                  // startActivity(intent)
+                  activityMoodListUpdate.launch(intent)
                 }
                 else {
-                    MooDoClient.retrofit.userMoodListCheck(userId, selectDate).enqueue(object:retrofit2.Callback<Boolean> {
-                        override fun onResponse(call: Call<Boolean>, response: Response<Boolean>) {
-                            if (response.isSuccessful) {
-                                if (response.body() == true) {
-                                    intent.putExtra("userId", userId)
-                                    intent.putExtra("selectDate", selectDate)
-                                    val stats = "insert"
-                                    intent.putExtra("stats", stats)
-
-                                    // startActivity(intent)
-                                    activityMoodListUpdate.launch(intent)
-                                }
-                                else {
-                                    AlertDialog.Builder(binding.root.context)
-                                        .setMessage("이미 작성된 일기입니다.")
-                                        .setPositiveButton("확인", null)
-                                        .show()
-                                }
-                            }
-                        }
-
-                        override fun onFailure(call: Call<Boolean>, t: Throwable) {
-                            Log.d("MooDoLog modeF", t.toString())
-                        }
-
-                    })
+                  AlertDialog.Builder(binding.root.context)
+                    .setMessage("이미 작성된 일기입니다.")
+                    .setPositiveButton("확인", null)
+                    .show()
                 }
+              }
             }
-            catch(e:Exception) {
-                e.printStackTrace()
-                Log.d("MooDoLog ModeMove Error", e.toString())
+
+            override fun onFailure(call: Call<Boolean>, t: Throwable) {
+              Log.d("MooDoLog modeF", t.toString())
             }
+
+          })
         }
-
-
-        // 한달 통계에서 다시 복귀할때
-        val activityStatisMood = registerForActivityResult(ActivityResultContracts.StartActivityForResult()) {result ->
-            if (result.resultCode == RESULT_OK) {
-                val update = result.data?.getBooleanExtra("update", false) ?: false
-                if (update) {
-                    monthAdapter.notifyDataSetChanged()
-                }
-            }
-        }
-        // 한달 통계
-        binding.statisBtn.setOnClickListener {
-            val intent = Intent(this@MainActivity_MooDo, MainActivity_Statis::class.java)
-
-            val selectDate = saveDate.text.toString()
-
-            intent.putExtra("userId", userId)
-            intent.putExtra("selectDate", selectDate)
-
-            // startActivity(intent)
-
-            activityStatisMood.launch(intent)
-        }
+      }
+      catch(e:Exception) {
+        e.printStackTrace()
+        Log.d("MooDoLog ModeMove Error", e.toString())
+      }
     }
 
-    override fun onResume() {
-        super.onResume()
-        val date = binding.saveDate.text.toString()
-        refreshTodoList(date)
+
+    // 한달 통계에서 다시 복귀할때
+    val activityStatisMood = registerForActivityResult(ActivityResultContracts.StartActivityForResult()) {result ->
+      if (result.resultCode == RESULT_OK) {
+        val update = result.data?.getBooleanExtra("update", false) ?: false
+        if (update) {
+          monthAdapter.notifyDataSetChanged()
+        }
+      }
     }
+    // 한달 통계
+    binding.statisBtn.setOnClickListener {
+      val intent = Intent(this@MainActivity_MooDo, MainActivity_Statis::class.java)
 
-    private fun refreshTodoList(date:String){
-        val userId = intent.getStringExtra("id").toString()
+      val selectDate = saveDate.text.toString()
 
-        MooDoClient.retrofit.getTodoListN(userId, date).enqueue(object : retrofit2.Callback<List<MooDoToDo>> {
-            override fun onResponse(call: Call<List<MooDoToDo>>, response: Response<List<MooDoToDo>>) {
-                if (response.isSuccessful) {
-                    val todoList = response.body() ?: mutableListOf()
-                    val todoAdapter = binding.todoListLayout.adapter as CalendarToDoAdapter
-                    todoAdapter.todoList.clear()
-                    todoAdapter.todoList.addAll(todoList)
-                    todoAdapter.notifyDataSetChanged()
-                } else {
-                    Log.d("MooDoLog", "Response is not successful: ${response.code()}")
-                }
-            }
+      intent.putExtra("userId", userId)
+      intent.putExtra("selectDate", selectDate)
 
-            override fun onFailure(call: Call<List<MooDoToDo>>, t: Throwable) {
-                Log.d("MooDoLog getTodo Fail", t.toString())
-            }
-        })
+      // startActivity(intent)
+
+      activityStatisMood.launch(intent)
     }
+  }
+
+  override fun onResume() {
+    super.onResume()
+    val date = binding.saveDate.text.toString()
+    refreshTodoList(date)
+  }
+
+  private fun refreshTodoList(date:String){
+    val userId = intent.getStringExtra("id").toString()
+
+    MooDoClient.retrofit.getTodoListN(userId, date).enqueue(object : retrofit2.Callback<List<MooDoToDo>> {
+      override fun onResponse(call: Call<List<MooDoToDo>>, response: Response<List<MooDoToDo>>) {
+        if (response.isSuccessful) {
+          val todoList = response.body() ?: mutableListOf()
+          val todoAdapter = binding.todoListLayout.adapter as CalendarToDoAdapter
+          todoAdapter.todoList.clear()
+          todoAdapter.todoList.addAll(todoList)
+          todoAdapter.notifyDataSetChanged()
+        } else {
+          Log.d("MooDoLog", "Response is not successful: ${response.code()}")
+        }
+      }
+
+      override fun onFailure(call: Call<List<MooDoToDo>>, t: Throwable) {
+        Log.d("MooDoLog getTodo Fail", t.toString())
+      }
+    })
+  }
 }
